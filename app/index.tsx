9a398e5ad141a01eae4b5781b071930bb92b7ee9
Selim Kälin <selim.kaelin@ipt.ch>
2025-10-01 17:38:17 +0200

    feat: add background image

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,28 @@
+import { ImageBackground } from "expo-image";
 import { Stack } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 export default function Index() {
   return (
     <>
-    <Stack.Screen options={{ headerShown: false }}/>
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.title}>Hello there!</Text>
-      <Text style={styles.subtitle}>More is coming soon, maybe ... when I find time!</Text>
-    </View>
+    <Stack.Screen options={{ title: "limitless", headerShown: false }}/>
+    <ImageBackground
+    source={require("../assets/images/background.jpg")}
+    style={styles.background}
+    contentFit="fill">
+    <Text style={styles.title}>Hello there!</Text>
+    <Text style={styles.subtitle}>More is coming soon, maybe ... when I find time!</Text>
+    </ImageBackground>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   container: {
     flex: 1,
     backgroundColor: "#f8fafc", // light background 
@@ -26,13 +33,13 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
     fontWeight: "700",
-    color: "#1e293b", // dark slate
+    color: "#f8fafc", // dark slate
     marginBottom: 12,
   },
   subtitle: {
     fontSize: 18,
     fontWeight: "400",
-    color: "#475569", // modern gray
+    color: "#bec7d2ff", // modern gray
     textAlign: "center",
     lineHeight: 26,
   },
