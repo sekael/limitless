a20aa73de49ae41b2b6c73e59f3b5492f0c364fb
Selim Kälin <selim.kaelin@ipt.ch>
2025-10-23 08:09:57 +0200

    fix: display of quote pill and zone handling

--- lib/features/quotes/presentation/quote_display.dart
+++ lib/features/quotes/presentation/quote_display.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:limitless_flutter/features/quotes/domain/quote.dart';
+import 'package:limitless_flutter/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class QuoteDisplay extends StatelessWidget {
   final Quote quote;
@@ -11,52 +13,34 @@ class QuoteDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      quote.text.toString(),
-      textAlign: TextAlign.center,
-      style: GoogleFonts.merriweather(
-        fontStyle: FontStyle.italic,
-        fontSize: 32,
-        height: 1.3,
-        fontWeight: FontWeight.w400,
-        color: Theme.of(context).colorScheme.onSurface,
+    final isDark = context.watch<ThemeProvider>().isDark;
+    final borderColor = isDark
+        ? Colors.white.withAlpha(64)
+        : Colors.grey.withAlpha(64);
+    final backgroundColor = isDark
+        ? Colors.white.withAlpha(32)
+        : Colors.grey.withAlpha(32);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(20.0),
+        border: Border.all(color: borderColor),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+        child: Text(
+          quote.text.toString(),
+          textAlign: TextAlign.center,
+          style: GoogleFonts.merriweather(
+            fontStyle: FontStyle.italic,
+            fontSize: 32,
+            height: 1.3,
+            fontWeight: FontWeight.w400,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
       ),
     );
   }
 }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return ClipRRect(
-  //     borderRadius: BorderRadius.circular(20.0),
-  //     child: Stack(
-  //       children: [
-  //         BackdropFilter(
-  //           filter: ImageFilter.blur(sigmaX: 8.0, sigmaY: 8.0),
-  //           child: const SizedBox.expand(),
-  //         ),
-  //         Container(
-  //           padding: const EdgeInsets.symmetric(
-  //             vertical: 16.0,
-  //             horizontal: 8.0,
-  //           ),
-  //           decoration: BoxDecoration(
-  //             color: Colors.white.withAlpha(32),
-  //             borderRadius: BorderRadius.circular(20.0),
-  //             border: Border.all(color: Colors.white.withAlpha(40)),
-  //           ),
-  //           child: Text(
-  //             quote.text.toString(),
-  //             textAlign: TextAlign.center,
-  //             style: GoogleFonts.merriweather(
-  //               fontStyle: FontStyle.italic,
-  //               fontSize: 32,
-  //               height: 1.3,
-  //               fontWeight: FontWeight.w400,
-  //               color: Theme.of(context).colorScheme.onSurface,
-  //             ),
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //   );
