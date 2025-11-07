45a532435a5187b7ee2d18c2d04f92d3302a7d67
Selim Kälin <selim.kaelin@ipt.ch>
2025-11-07 09:29:12 +0100

    feat: add secondary buttons, make login flow pretty

--- lib/components/buttons/secondary_adaptive.dart
+++ lib/components/buttons/secondary_adaptive.dart
@@ -3,8 +3,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:limitless_flutter/components/buttons/glass_surface.dart';
 
-class AdaptiveButton extends StatelessWidget {
-  const AdaptiveButton({super.key, required this.buttonText, this.onPressed});
+class SecondaryAdaptiveButton extends StatelessWidget {
+  const SecondaryAdaptiveButton({
+    super.key,
+    required this.buttonText,
+    this.onPressed,
+  });
 
   final String buttonText;
   final VoidCallback? onPressed;
@@ -19,8 +23,8 @@ class AdaptiveButton extends StatelessWidget {
     final cs = Theme.of(context).colorScheme;
 
     // Glass-like colors
-    final baseColor = cs.primary;
-    final glassColor = baseColor.withValues(alpha: 0.35);
+    final baseColor = cs.inversePrimary;
+    final glassColor = baseColor.withValues(alpha: 0.05);
     final borderColor = baseColor.withValues(alpha: 0.45);
     final highlightStroke = Colors.white.withValues(alpha: 0.15);
     final radius = BorderRadius.circular(14);
